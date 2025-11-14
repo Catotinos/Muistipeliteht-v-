6052a7cc1fcfd999bbc773755d3e9fceed063468
Arttu Hasa <arttuhasa@gmail.com>
2025-11-14 10:20:30 +0200

    nyt pelissä on reset ja lopetus funktio

--- js/board.js
+++ js/board.js
@@ -4,27 +4,31 @@ const allCards = [
     '🍎', '🍐', '🍒', '🍉', '🍇', '🍓', '🍌', '🍍', '🥝', '🥥', '🍑', '🍈', '🍋', '🍊', '🍏', '🍅'
 ];
 const gameBoard = document.getElementById('game-board');
-const  resetbutton = document.getElementById("reset");
-resetbutton.addEventListener('click', reset());
+const resetbutton = document.getElementById("reset");
+const resetdiv = document.getElementById("btn");
+resetbutton.addEventListener('click', reset);
 let firstCard = null;
 let secondCard = null;
 let lockBoard = false;
+const cardsfound = [];
+const cardsonboard = [];
 
 function shuffle(array) {
     let i = array.length;
     while (i != 0) {
         let randomi = Math.floor(Math.random() * i);
         i--;
-        [array[i], array[randomi]] = [
-        array[randomi], array[i]];
+        [array[i], array[randomi]] = [array[randomi], array[i]];
   }    
 }
 
 export function createBoard(cardCount) {
     const selectedCards = allCards.slice(0, cardCount / 2);
     const cards = [...selectedCards, ...selectedCards];
+    resetdiv.style.display = "none"
     shuffle(cards);
     cards.forEach(card => {
+        cardcounter(card);
         const cardElement = createCardElement(card);
         cardElement.addEventListener('click', () => flipCard(cardElement, handleCardFlip, lockBoard));
         gameBoard.appendChild(cardElement);
@@ -55,7 +59,15 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
-    resetBoard();
+    cardsfound.push(firstCard);
+    cardsfound.push(secondCard);
+    if(cardsfound.length == cardsonboard.length){
+        resetBoard();
+        ending();
+    } else {
+        resetBoard();
+    }
+    
 }
 
 function unflipCards() {
@@ -73,7 +85,23 @@ function resetBoard() {
     [firstCard, secondCard, lockBoard] = [null, null, false];
 }
 
+function cardcounter(card) {
+    cardsonboard.push(card);
+
+}
 
 function reset() {
-    console.log("toimii")
+    const cardCount = parseInt(prompt("Syötä korttien määrä (parillinen luku):"), 10);
+    if (cardCount % 2 !== 0) {
+        alert("Korttien määrän täytyy olla parillinen luku.");
+        return;
+    }
+    createBoard(cardCount);
+}
+
+function ending() {
+    while (gameBoard.firstChild) {
+        gameBoard.removeChild(gameBoard.lastChild);
+    }
+    resetdiv.style.display = "block"
 }
